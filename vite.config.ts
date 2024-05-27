ef197a32f01d02d4c821e3273e59e27e82d2d6cb
mpereannor <mpereannor@gmail.com>
2024-05-27 19:13:53 +0000

    chore: update tailwind, vite and shadcn ui configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./@"),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
